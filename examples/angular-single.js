a6f514203c9134e5f66196b7c86b4f588bb1dc4c
Alexandru Badiu <andu@ctrlz.ro>
2013-12-04 18:33:32 +0200

    Updated angular examples.

--- examples/angular-single.js
+++ examples/angular-single.js
@@ -9,4 +9,8 @@ app.controller('MainCtrl', function($scope) {
     { id: 3, name: 'C' },
     { id: 4, name: 'D' }
   ];
+
+  $scope.add = function () {
+    $scope.data1.push({id: $scope.data1.length + 1, name: 'E'});
+  };
 });
