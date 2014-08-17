22d0e707b57cb726dcf293defa2136c3f176c10d
Alexandru Badiu <andu@ctrlz.ro>
2014-08-18 01:48:25 +0300

    Updated example.

--- examples/angular-connected.js
+++ examples/angular-connected.js
@@ -20,4 +20,22 @@ app.controller('MainCtrl', function($scope) {
     { id: 3, name: 'C' },
     { id: 4, name: 'D' }
   ];
+  
+  $('ul').bind('dragover', function (event) {
+    var $dropZone = $(this), $timeout = window.dropZoneTimeout;
+    var $hClass = 'hover';
+
+    if (!$timeout) {
+      $dropZone.addClass($hClass);
+    } else {
+      clearTimeout($timeout);
+    }
+    
+    window.dropZoneTimeout = setTimeout(function () {
+      window.dropZoneTimeout = null;
+      $dropZone.removeClass($hClass);
+    }, 100);
+
+    event.preventDefault();
+  });
 });
