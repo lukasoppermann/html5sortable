3caa1a7f75f7e7a952127658552ac009bd713cd8
Yuval Shafir <yshafir@slideshare.com>
2015-06-26 14:59:37 -0700

    built the new src

--- dist/html.sortable.js
+++ dist/html.sortable.js
@@ -298,6 +298,7 @@ var sortable = function(selector, options) {
       // trigger sortstar update
       dragging.parent().triggerHandler('sortstart', {
         item: dragging,
+        placeholder: placeholder,
         startparent: startParent
       });
     });
@@ -375,7 +376,10 @@ var sortable = function(selector, options) {
           }
         }
 
-        dragging.hide();
+        if(dragging.is(':visible')){
+          dragging.hide();
+          dragging.appendTo($(dragging).parent());
+        }
         if (placeholder.index() < $(this).index()) {
           $(this).after(placeholder);
         } else {
