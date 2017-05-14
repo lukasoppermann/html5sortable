5feb85e91a10b25496725c3e54abfe87d632a6a7
Lukas Oppermann <lukas@vea.re>
2017-05-14 18:48:18 +0200

    working on upgrade

--- test/umd/module.js
+++ test/umd/module.js
@@ -1,37 +1,26 @@
 /* global describe,before,it */
 // testing the UMD (module loading capabilities)
 describe('Testing UMD (Universal Module Definition) against /dist/html.sortable.js', function () {
-  var assert = require('chai').assert
-  var path = require('path')
+  let assert = require('chai').assert
+  let path = require('path')
+  const { JSDOM } = require('jsdom')
+  const sortable = require('fs').readFileSync('./src/html.sortable.js', { encoding: 'utf-8' })
 
   describe('Assignment to global variable', function () {
-    var test = {}
-    before(function (done) {
-      global.sortable = undefined
-      require('jsdom').env({
-        html: '<html><body></body></html>',
-        scripts: [
-          path.resolve(__dirname, '../../dist/html.sortable.js')
-        ],
-        done: function (errors, window) {
-          test.sortable = window.sortable
-          done()
-        }
-      })
+    before(function () {
+      window = (new JSDOM(``, { runScripts: 'dangerously' })).window
+      // Execute my library by inserting a <script> tag containing it.
+      const scriptEl = window.document.createElement('script')
+      scriptEl.textContent = sortable
+      window.document.head.appendChild(scriptEl)
     })
 
     it('sortable should be defined as a function', function () {
-      assert.typeOf(test.sortable, 'function')
+      assert.typeOf(window.sortable, 'function')
     })
   })
 
   describe('CommonJS Module', function () {
-    before(function () {
-      global.document = require('jsdom').jsdom('<html lang="en-US"></html>')
-      global.window = global.document.defaultView
-      global.sortable = undefined
-    })
-
     it('should be able to require html.sortable', function () {
       var sortable = require('../../dist/html.sortable.js')
       assert.typeOf(sortable, 'function')
@@ -39,28 +28,15 @@ describe('Testing UMD (Universal Module Definition) against /dist/html.sortable.
   })
 
   describe('AMD (Asynchronous module definition)', function () {
-    var test = {}
-    before(function (done) {
-      global.sortable = undefined
-      require('jsdom').env({
-        file: path.resolve(__dirname, 'amd.html'),
-        features: {
-          FetchExternalResources: ['script'],
-          ProcessExternalResources: ['script']
-        },
-        done: function (errors, window) {
-          test.requirejs = window.require
-          setTimeout(function () {
-            // timeout is needed so requirejs can load the resources
-            test.sortable = window.sortable
-            done()
-          }, 200)
-        }
+    before(function () {
+      window = JSDOM.fromFile('./test/umd/amd.html', { runScripts: 'dangerously' }).then(dom => {
+
       })
     })
 
     it('should define sortable', function () {
-      assert.typeOf(test.sortable, 'function')
+      console.log(window)
+      assert.typeOf(window.requiredSortable, 'function')
     })
   })
 })
