05c0205d9faf215447d28869d9ef2bd3ea27eac2
Lukas Oppermann <lukas@vea.re>
2018-03-01 22:13:08 +0100

    adding more tests

--- src/insertHtmlElements.ts
+++ src/insertHtmlElements.ts
@@ -4,6 +4,10 @@
  * @param {Element} element
  */
 let insertBefore = (target, element) => {
+  if (!target || target.nodeType !== 1 || !element || element.nodeType !== 1) {
+    throw new Error('target and element must be a node')
+  }
+
   target.parentElement.insertBefore(
     element,
     target
