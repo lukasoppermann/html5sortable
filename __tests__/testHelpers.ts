625aaa2d5b2e40a0207482fe8ed3a569cc92ef0f
Jerry <jerrymuzsik@icloud.com>
2018-03-08 14:21:45 -0500

    refactor - internal test file was put into a new folder that separated the functionality

--- __tests__/testHelpers.ts
+++ __tests__/testHelpers.ts
@@ -1,5 +1,3 @@
-// Here I will set document body's inner html to this string
-
 export const mockInnerHTML = `<ul class="sortable">
 <li class="li-first">
   <span class="handle"></span>
@@ -14,9 +12,3 @@ export const mockInnerHTML = `<ul class="sortable">
   item 3
 </li>
 </ul>`
-
-// // select sortable
-// ul = global.body.querySelector('.sortable')
-// li = ul.querySelector('.item-first')
-// secondLi = ul.querySelector('.item-second')
-// thirdLi = ul.querySelector('.item-second')
