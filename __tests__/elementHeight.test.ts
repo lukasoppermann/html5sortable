a61a3bb747c8dbc8ae6184da26d306c82e1a884b
Lukas Oppermann <oppermann.lukas@googlemail.com>
2018-03-11 20:21:35 +0100

    Extract element height (#364)
    * extracting elementHeight
    
    * add tests

---
+++ __tests__/elementHeight.test.ts
@@ -0,0 +1,28 @@
+/* global describe,test,expect */
+import elementHeight from '../src/elementHeight'
+
+describe('Testing elementHeight', () => {
+  test('no valid element', () => {
+    // assert
+    expect(() => { elementHeight('') }).toThrow('You must provide a valid dom element')
+  })
+
+  test('element is not in DOM', () => {
+    // setup
+    let div = window.document.createElement('div')
+    div.style.height = '10px'
+    // assert
+    expect(elementHeight(div)).toEqual(10)
+  })
+
+  test('element is in DOM', () => {
+    // setup
+    let div = window.document.createElement('div')
+    div.style.height = '10px'
+    div.style.paddingTop = '5px'
+    div.style.paddingBottom = '7px'
+    window.document.body.appendChild(div)
+    // assert
+    expect(elementHeight(document.querySelector('div'))).toEqual(22)
+  })
+})
