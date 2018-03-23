4e97f233e2a9352c1ee60ee354e5d77f568537a2
Lukas Oppermann <oppermann.lukas@gmail.com>
2018-03-23 11:38:37 +0100

    add toDo tests

---
+++ __tests__/store/storeData.test.ts
@@ -0,0 +1,29 @@
+/* global describe,test,expect */
+import store from '../../src/store'
+
+describe('Testing data store', () => {
+  test('set with invalid key', () => {
+
+  })
+  
+  test.skip('set with valid key', () => {
+
+  })
+  
+  test.skip('get not exisiting key', () => {
+
+  })
+  
+  test.skip('get exisiting key', () => {
+
+  })
+  
+  test.skip('delete not exisiting key', () => {
+
+  })
+  
+  test.skip('delete with exisiting key', () => {
+
+  })
+
+})
