dc6e9c1e391b87f2a528fb82b13498accd9063a4
Lukas Oppermann <lukas@vea.re>
2018-10-08 19:42:06 +0200

    merge ie11 fix

--- dist/html5sortable.cjs.js
+++ dist/html5sortable.cjs.js
@@ -255,10 +255,10 @@ function offset (element) {
     }
     var rect = element.getClientRects()[0];
     return {
-        left: rect.left + window.scrollX,
-        right: rect.right + window.scrollX,
-        top: rect.top + window.scrollY,
-        bottom: rect.bottom + window.scrollY
+        left: rect.left + window.pageXOffset,
+        right: rect.right + window.pageXOffset,
+        top: rect.top + window.pageYOffset,
+        bottom: rect.bottom + window.pageYOffset
     };
 }
 
