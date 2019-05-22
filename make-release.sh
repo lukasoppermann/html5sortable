214f7595761a3420f039e4e289e30854e6440755
Lukas Oppermann <lukas@vea.re>
2019-05-22 08:59:47 +0200

    working on release script

--- make-release.sh
+++ make-release.sh
@@ -3,17 +3,17 @@ git checkout master
 git pull
 
 # Remove our former release branch
-git branch -D lastest-release 2>/dev/null
-git push origin --delete lastest-release 2>/dev/null
+git branch -D latest-release 2>/dev/null
+git push origin --delete latest-release 2>/dev/null
 
 # Create a new branch to run the build under
-git checkout -b lastest-release
+git checkout -b latest-release
 
 # Ensure we have the latest version of things
 rm -rf node_modules # package-lock.json <-- may want to remove this file too if it suits your project.
-npm install 
+npm install
 
-# Test validity 
+# Test validity
 npm test
 
 # Build and update docs
@@ -29,7 +29,8 @@ echo "$newIgnore" > .gitignore # the redirect here is put into a spereate step t
 git add -A && git commit -m "[BUILD] $releaseVersion"
 
 # Make a new tag off of the latest build
-git checkout master
-git tag "$releaseVersion" lastest-release
+git tag "$releaseVersion" latest-release
 git push origin "$releaseVersion"
-git push origin lastest-release
+git push origin latest-release
+git checkout master
+#npm publish "https://github.com/lukasoppermann/html5sortable/archive/$releaseVersion.tar.gz"
